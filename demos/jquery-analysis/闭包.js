9c7f81bbc1094a10500bde053e8fdc5a5c185826
ZhangRZ <occultskyrong@163.com>
2018-01-09 23:14:50 +0800

    更新 - jquery

---
+++ demos/jquery-analysis/闭包.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zhangrz on 2018/1/3.
+ * Copyright© 2015-2020 occultskyrong (https://github.com/occultskyrong)
+ * @version 0.0.1 created
+ */
+
+'use strict';
+
+const a = 1;
+const b = 2;
+
+(function fun(aa, bb, cc) {
+    console.info(aa, bb, cc);
+}(a, b));
